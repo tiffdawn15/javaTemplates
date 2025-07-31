8eec2ad7ef76deaa6b8e807dccfe3977603f6469
Tiffany Messer <tiffanymesser15@gmail.com>
2025-07-31 09:30:44 -0400

    Adding a springboot repo

---
+++ fun/src/test/java/com/playground/fun/FunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playground.fun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
